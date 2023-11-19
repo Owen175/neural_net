bb2a94b392e40ae50646f9f92a46c2e3f4bfa4bc
Owen175 <oclocliphone@icloud.com>
2023-11-19 19:39:54 +0000

    fully implemented network apart from backpropogation

--- main.py
+++ main.py
@@ -1,4 +1,26 @@
 from mnist_data_handler import get_training_data, get_testing_data
+from net import NN
+training_images, training_labels = get_training_data()
+class Data:
+    def __init__(self, inputs, prediction):
+        self.inputs = inputs
+        temp = prediction
+        self.prediction = [0] * 9
+        self.prediction.insert(temp, 1)
+
+data_list = []
 
 training_images, training_labels = get_training_data()
+for image, label in zip(training_images, training_labels):
+    data_list.append(Data(image, label))
 
+nn = NN([784, 200, 10])
+nn.get_cost(data_list[0])
+count = 0
+for data in data_list:
+    nn.training(data, 20)
+    print(count)
+    count += 1
+    if count == 10:
+        print(nn.get_cost(data))
+print(nn.get_cost(data_list[0]))
--- net.py
+++ net.py
@@ -1,17 +1,26 @@
 import math
+import random
 
 
 class Layer:
     def __init__(self, num_inputs, num_outputs):
         self.biases = []
+        self.bias_gradient = []
+        for i in range(num_outputs):
+            self.biases.append(0)
+            self.bias_gradient.append(0)
         self.weights = []
+        self.weight_gradient = []
+        for i in range(num_inputs):
+            self.weights.append([])
+            self.weight_gradient.append([])
+            for j in range(num_outputs):
+                self.weights[-1].append(random.randint(-1000, 1000)/1000)
+                self.weight_gradient[-1].append(0)
+
         self.inputs = num_inputs
         self.outputs = num_outputs
 
-        for _ in range(num_inputs):
-            self.weights.append([])
-            for _ in range(num_outputs):
-                self.weights[-1].append()
         # Can be referenced as self.weights[input_node_num][output_node_num] to find the weight.
 
 
@@ -21,6 +30,10 @@ class NN:
         self.layers = [Layer(layer_dimensions[i], layer_dimensions[i + 1]) for i in range(len(layer_dimensions) - 1)]
 
     def sigmoid_activation_function(self, value):
+        if value < -100:
+            value = -100
+        elif value > 100:
+            value = 100
         return 1/(1+math.e**(-value))
 
     def calculate_layer(self, layer, inputs):
@@ -33,11 +46,65 @@ class NN:
             processed_outputs.append(self.sigmoid_activation_function(node_total))
         return processed_outputs
 
-    def process_information(self, inputs):
+    def testing(self, data):
+        inputs = data.inputs
         for layer in self.layers:
             inputs = self.calculate_layer(layer, inputs)
         return inputs
 
+    def training(self, data, learnrate):
+        self.calculate_gradients(learnrate, data)
 
-nn = NN([2, 2])
-print(nn.process_information([1, 2]))
+    def get_cost(self, data):
+        outputs = self.testing(data)
+        cost = 0
+        try:
+            for output, expected_output in zip(outputs, data.prediction):
+                cost += (output - expected_output)**2
+        except Exception as e:
+            print(e, outputs, data.prediction)
+            exit()
+        return cost
+
+    def individual_node_cost_derivative(self, output, expected_output):
+        return 2 * (output - expected_output)
+
+    def sigmoid_activation_derivative(self, input):
+        activation = self.sigmoid_activation_function(input)
+        return activation * (1-activation)
+
+
+
+    def update_weights_and_biases(self, layer, learn_rate):
+        for i in range(layer.outputs):
+            layer.biases[i] -= layer.bias_gradient[i] * learn_rate
+            for j in range(layer.inputs):
+                layer.weights[j][i] = layer.weight_gradient[j][i] * learn_rate
+
+    def calculate_gradients(self, learnrate, data):
+        h = 0.000001
+        cost = self.get_cost(data)
+
+        for layer in self.layers:
+            for i in range(layer.inputs):
+                for j in range(layer.outputs):
+                    layer.weights[i][j] += h
+                    change_in_cost = self.get_cost(data) - cost
+                    layer.weights[i][j] -= h
+                    layer.weight_gradient[i][j] = change_in_cost / h
+            for i in range(layer.outputs):
+                layer.biases[i] += h
+                change_in_cost = self.get_cost(data) - cost
+                layer.biases[i] -= h
+                layer.bias_gradient[i] = change_in_cost / h
+
+        for layer in self.layers:
+            self.update_weights_and_biases(layer, learnrate)
+
+# data = Training_Data([1, 2], [0, 1])
+# nn = NN([2, 20, 2])
+# nn.get_cost(data)
+# for i in range(1000):
+#     nn.training(data, 20)
+#
+# print(nn.testing(data))
